afb8e221cdd48f5feee6b3b0d48d1293176f5f36
杨东雄 <2545493686@qq.com>
2018-12-16 23:22:22 +0800

    添加项目文件。

---
+++ SpiderLibrary/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using LanQ.HttpLibrary;
+
+namespace LanQ.SpiderLibrary
+{
+    public class Spider
+    {
+        public string[] UrlList { get; }
+        public Spider(string[] url)
+        {
+            UrlList = url;
+        }
+
+        public List<string>[] GetText(string[] regexPatternes)
+        {
+            List<string>[] ret = new List<string>[regexPatternes.Length];
+
+            for (int i = 0; i < regexPatternes.Length; i++)
+            {
+                string source = Http.Get(UrlList[i]);
+                MatchCollection matches = Regex.Matches(source, regexPatternes[i]);
+
+                ret[i] = new List<string>();
+
+                foreach (Match item in matches)
+                {
+                    ret[i].Add(item.Value);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
