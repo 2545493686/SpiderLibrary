e7c7ab4ed03502b850b5c83464cc5d2282284d2e
杨东雄 <2545493686@qq.com>
2018-12-17 19:29:21 +0800

    添加剔除

--- SpiderLibrary/Spider.cs
+++ SpiderLibrary/Spider.cs
@@ -9,8 +9,16 @@ using LanQ;
 
 namespace LanQ.SpiderLibrary
 {
+    public struct Pattern
+    {
+        public string pattern;
+        public string startTrim;
+        public string endTrim;
+    }
+
     public class Spider
     {
+        
         public string[] UrlList { get; }
         protected TextFile TextFile { get; }
 
@@ -20,35 +28,46 @@ namespace LanQ.SpiderLibrary
             TextFile = textFile;
         }
 
-        public List<string>[] GetText(string[] regexPatternes, bool logInConsole = true)
+        public List<string[]>[] GetText(Pattern[] patternes, bool logInConsole = true)
         {
             if (logInConsole)
                 Console.WriteLine("Spider Start!");
 
-            List<string>[] ret = new List<string>[regexPatternes.Length];
+            List<string[]>[] ret = new List<string[]>[UrlList.Length];
 
-            for (int i = 0; i < regexPatternes.Length; i++)
+            for (int i = 0; i < UrlList.Length; i++)
             {
                 if (logInConsole)
                     Console.WriteLine("Spider[{0}]: {1}", i, UrlList[i]);
 
-                string source = Http.Get(UrlList[i]);
-                MatchCollection matches = Regex.Matches(source, regexPatternes[i]);
+                string source = Http.Get(UrlList[i]); //获得当前url的源
+                ret[i] = new List<string[]>();
 
-                ret[i] = new List<string>();
+                List<MatchCollection> matches = new List<MatchCollection>();
+
+                foreach (Pattern pattern in patternes)
+                {
+                    matches.Add(Regex.Matches(source, pattern.pattern)); //获得匹配项
+                }
+
+                int count = matches.Count > 0 ? matches[0].Count : 0;
+                for (int j = 0; j < count; j++)
+                {
+                    ret[i].Add(new string[matches.Count]);
+                    for (int k = 0; k < matches.Count; k++)
+                    {
+                        
+                        ret[i][j][k] = matches[k][j].Value.Trim(patternes[k].startTrim, patternes[k].endTrim);
+
+                        if (TextFile != null) //文本属性存在时写入文件
+                        {
+                            TextFile.WriteLine(matches[k][j].Value.Trim(patternes[k].startTrim, patternes[k].endTrim));
+                        }
+                    }
+                }
 
                 if (logInConsole)
-                    Console.WriteLine("Spider[{0}]: success get {1}", i, matches.Count);
-
-                foreach (Match item in matches)
-                {
-                    if (TextFile == null) //文本属性存在时写入文件
-                    {
-                        TextFile.WriteLine(item.Value);
-                    }
-
-                    ret[i].Add(item.Value);
-                }
+                    Console.WriteLine("Spider[{0}]: success get {1}", i, count);
             }
 
             if (logInConsole)
